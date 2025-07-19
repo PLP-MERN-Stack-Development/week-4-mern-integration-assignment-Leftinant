c4eef3c9bedc5244b4d9cba1fdf6c6acc66f7b51
Benjamin <157468010+Leftinant@users.noreply.github.com>
2025-07-19 12:12:07 +0300

    Add files via upload

---
+++ client/vite.config.js
@@ -0,0 +1,18 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000", // <-- your Express backend port
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
